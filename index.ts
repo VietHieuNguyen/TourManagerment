a139cb7804097a37339990d94fa5cb4bb9f23a14
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-04-02 17:20:18 +0700

    B46 1 1.3. Pug

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express, {Request, Response, Express} from "express"
 const app: Express = express()
 const port:number = 3000 
+
+app.set("views", "./views")
+app.set("view engine", "pug")
 app.get("/", (req: Request, res: Response)=>{
   res.send("ok")
 })
 app.get("/tours", (req: Request, res: Response)=>{
-  res.send("các tour")
+  res.render("client/pages/tours/index")
 })
 app.listen(port, ()=>{
   console.log(`App listening on port: ${port}`)
