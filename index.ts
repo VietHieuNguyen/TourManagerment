69c1e9bad9edc9175fbbc9b302edc14030533561
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-04-04 09:54:11 +0700

    B46 2 1.5. Lấy ra danh sách tour

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response, Express} from "express"
 import sequelize from "./config/database"
 import dotenv from "dotenv"
+import Tour from "./models/tour.model"
 dotenv.config()
 const app: Express = express()
 const port:number|string = process.env.PORT || 3000
@@ -10,8 +11,14 @@ app.set("view engine", "pug")
 app.get("/", (req: Request, res: Response)=>{
   res.send("ok")
 })
-app.get("/tours", (req: Request, res: Response)=>{
-  res.render("client/pages/tours/index")
+app.get("/tours",async  (req: Request, res: Response)=>{
+  const tours = await Tour.findAll({
+    raw: true
+  });
+  console.log(tours)
+  res.render("client/pages/tours/index",{
+    tours: tours
+  })
 })
 app.listen(port, ()=>{
   console.log(`App listening on port: ${port}`)
