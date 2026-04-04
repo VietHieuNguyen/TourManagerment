f21afb5cebf3f680a182554d44c227c23cef2960
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-04-04 10:22:26 +0700

    B46 2 2.4. Sửa layout website

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ const port:number|string = process.env.PORT || 3000
 app.set("views", "./views")
 app.set("view engine", "pug")
 
+app.use(express.static("public"))
+
 clientRoutes(app)
 app.listen(port, ()=>{
   console.log(`App listening on port: ${port}`)
