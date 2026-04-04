6c10878f70f41654d59c74983c6a307ac8b9ad19
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-04-04 22:14:37 +0700

    B47 1 2.1.Thêm sản phẩm giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -15,3 +15,42 @@ var imagesMain = new Swiper(".imagesMain", {
         swiper: imagesThumb,
       },
     });
+
+//end slider
+
+//Carts 
+//Nếu chưa có giỏ hàng trong localStorage thì tạo giỏ hàng
+const cart = localStorage.getItem("cart")
+if(!cart){
+  localStorage.setItem("cart",JSON.stringify([]))
+}
+
+
+//Thêm tour vào giỏ
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if(formAddToCart){
+  formAddToCart.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+    if(quantity > 0 && tourId){
+      //Lấy cart cũ 
+      const cart = JSON.parse(localStorage.getItem("cart"));
+      const indexExistTour = cart.findIndex(item => item.tourId == tourId);
+      if(indexExistTour==-1){
+        cart.push({
+        tourId: tourId,
+        quantity: quantity
+      });
+      }else{
+        cart[indexExistTour].quantity = cart[indexExistTour].quantity + quantity;
+      }
+      
+
+      localStorage.setItem("cart",JSON.stringify(cart))
+      
+    }
+  })
+}
+
+//Carts
