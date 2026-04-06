f54db4767c3d65cdcbb96414f3e84d1b56443b48
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-04-06 15:15:54 +0700

    B48 2 2.3. Server lấy data và lưu và order_item

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import Order from "../../models/order.model";
 import { generateOrderCode } from "../../helpers/generate";
+import Tour from "../../models/tour.model";
+import OrderItem from "../../models/order-item.model";
 export const order = async (req: Request, res: Response)=>{
   const data = req.body;
 
@@ -24,7 +26,44 @@ export const order = async (req: Request, res: Response)=>{
     where:{
       id: orderId
     }
-  })
+  });
+
+
+
+interface IData{
+  orderId: number;
+  tourId: number;
+  quantity: number;
+  price: number;
+  discount?: number;   // Dấu ? vì trong model bạn không để allowNull: false
+  timeStart: Date;
+}
+  //Lưu data vào order_items
+for(const item of data.cart){
+  const dataItem = {
+    orderId: orderId,
+    tourId: item.tourId,
+    quantity: item.quantity
+  } as unknown as IData
+ 
+  const infoTour = await Tour.findOne({
+    where:{
+      id: item.tourId,
+      deleted: false,
+      status: "active"
+    },
+    raw: true
+  }) as unknown as IData
+  if(infoTour){
+  dataItem["price"] = infoTour["price"]
+  dataItem["discount"] = infoTour["discount"]
+  dataItem["timeStart"] = infoTour["timeStart"]
+  }
+  await OrderItem.create(dataItem as any)
+  
+}
+
+  
   res.json({
     code: 200, 
     message:"Đặt hàng thành công!",
