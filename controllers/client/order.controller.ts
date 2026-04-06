a8a5710335c2b240d051a6f986d1bd6f86d8162c
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-04-06 11:48:30 +0700

    B48 2 2.2. Server lấy data và lưu vào bảng orders

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,9 +1,33 @@
 import { Request, Response } from "express"
-export const order = (req: Request, res: Response)=>{
+import Order from "../../models/order.model";
+import { generateOrderCode } from "../../helpers/generate";
+export const order = async (req: Request, res: Response)=>{
   const data = req.body;
-  console.log(data)
+
+
+  //Luư data vào bảng orders
+  const dataOrder = {
+    code: "",
+    fullName: data.info.fullName,
+    phone: data.info.phone,
+    note: data.info.note,
+    status: "initial"
+  }
+  
+  const order = await Order.create(dataOrder)
+
+  const orderId = order.dataValues.id
+  const code = generateOrderCode(orderId);
+  await Order.update({
+    code: code
+  },{
+    where:{
+      id: orderId
+    }
+  })
   res.json({
     code: 200, 
-    message:"Đặt hàng thành công!"
+    message:"Đặt hàng thành công!",
+    orderCode: code
   })
 }
